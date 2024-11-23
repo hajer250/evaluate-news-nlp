0756ac74bfac30480c7aa4304ae26b7fc94f4202
hajer250 <hajerzaher250@gmail.com>
2024-11-23 21:33:22 +0400

    Update index.js

--- starter_project/src/client/index.js
+++ starter_project/src/client/index.js
@@ -1,7 +1,19 @@
 // js files
+import { checkForName} from './js/nameCecker'
 import { handleSubmit } from './js/formHandler'
 
 
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/form.scss'
+import './styles/footer.scss'
+import './styles/header.scss'
+
+
+export {
+  checkFormName,
+  handlesubmit
+}
 
 // alert("I EXIST")
 // console.log("CHANGE!!");
